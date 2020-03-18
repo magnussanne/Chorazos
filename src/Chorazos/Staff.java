6213a06975bb655c4d57417b1e329fc3d2e49842
magnussanne <magnussanne@me.com>
2020-03-18 19:47:39 +0000

    staff data

---
+++ src/Chorazos/Staff.java
@@ -0,0 +1,19 @@
+package Chorazos;
+public class Staff {
+	
+	private String staffMember;
+	private String researchActivity;
+	private String researchArea;
+	private String specialFocus;
+	
+	public Staff(String staffMember, String researchActivity, String researchArea, String specialFocus){
+			this.staffMember = staffMember;
+			this.researchActivity = researchActivity;
+			this.researchArea = researchArea;
+			this.specialFocus = specialFocus;
+		}
+	@Override
+	public String toString() {
+		return "name: " +staffMember+ "Research Activity: "+researchActivity+ "Research Area: "+researchArea+ "Special Focus: "+specialFocus; 
+	}
+}
