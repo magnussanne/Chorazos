eabeacef7ea2f3ff593c6aa1c49a85010e4eaf62
magnussanne <magnussanne@me.com>
2020-03-18 20:38:42 +0000

    update

---
+++ src/Chorazos/ReadStaffMembers.java
@@ -0,0 +1,50 @@
+package Chorazos;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+ 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ReadStaffMembers {
+	 public static void main(String[] args) throws IOException {
+	        String excelFilePath = "Books.xlsx";
+	        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+	         
+	        Workbook workbook = new XSSFWorkbook(inputStream);
+	        Sheet firstSheet = workbook.getSheetAt(0);
+	        Iterator<Row> iterator = firstSheet.iterator();
+	         
+	        while (iterator.hasNext()) {
+	            Row nextRow = iterator.next();
+	            Iterator<Cell> cellIterator = nextRow.cellIterator();
+	             
+	            while (cellIterator.hasNext()) {
+	                Cell cell = cellIterator.next();
+	                 
+	                switch (cell.getCellType()) {
+	                    case Cell.CELL_TYPE_STRING:
+	                        System.out.print(cell.getStringCellValue());
+	                        break;
+	                    case Cell.CELL_TYPE_BOOLEAN:
+	                        System.out.print(cell.getBooleanCellValue());
+	                        break;
+	                    case Cell.CELL_TYPE_NUMERIC:
+	                        System.out.print(cell.getNumericCellValue());
+	                        break;
+	                }
+	                System.out.print(" - ");
+	            }
+	            System.out.println();
+	        }
+	         
+	        workbook.close();
+	        inputStream.close();
+	    }
+}
