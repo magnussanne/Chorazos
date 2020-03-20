40bb77b80a52d6a7eb534b9975f9467acec1da59
Conor Dunne <conoredunne@gmail.com>
2020-03-20 14:27:06 +0000

    Fixed reading from excel

--- src/main/java/ReadStaffMembers.java
+++ src/main/java/ReadStaffMembers.java
@@ -17,71 +17,71 @@ import java.util.List;
 public class ReadStaffMembers {
     private static final String File_Path = "src/main/resources/staff_input.xlsx";
 
-	public static void main(String[] args) throws IOException {
-        List staffList = getStaffDetails();
-        System.out.println(staffList);
-	}
-
-	 public static List getStaffDetails() throws IOException {
-		 List staffList = new ArrayList();
-		 FileInputStream fis = null;
-
-		 try {
-             fis = new FileInputStream(File_Path);
-             Workbook workbook = new XSSFWorkbook(fis);
-
-             int numberOfSheets = workbook.getNumberOfSheets();
-
-             for (int i = 0; i < numberOfSheets; i++) {
-                 Sheet sheet = workbook.getSheetAt(i);
-                 Iterator rowIterator = sheet.iterator();
-
-                 while (rowIterator.hasNext()) {
-
-
-                     Row row = (Row) rowIterator.next();
-                     Iterator cellIterator = row.cellIterator();
-
-                     while (cellIterator.hasNext()) {
-                         Staff staff;
-                         Cell cell = (Cell) cellIterator.next();
-
-                         String name = "";
-                         String activity = "";
-                         String area = "";
-                         Focus focus = null;
-                             if (cell.getColumnIndex() == 0) {
-                                 name = cell.getStringCellValue();
-                             }
-                             else if (cell.getColumnIndex() == 1) {
-                                 activity = cell.getStringCellValue();
-                             }
-                             else if (cell.getColumnIndex() == 2) {
-                                 area = cell.getStringCellValue();
-                             }
-                             else if (cell.getColumnIndex() == 3) {
-                                 String f = cell.getStringCellValue();
-
-                                 if (f.equals("Dagon Studies"))
-                                     focus = Focus.DS;
-                                 else {
-                                     focus = Focus.CS;
-                                 }
-                             }
-
-                         staff = new Staff(name, activity, area, focus);
-                         staffList.add(staff);
-                     }
-                 }
-             }
-
-             fis.close();
-		 } catch (FileNotFoundException e) {
-            e.printStackTrace();
-		 } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        List<Staff> staffList = getStaffDetails();
+        Staff s = staffList.get(2);
+        System.out.println(s.toString());
     }
 
-         return staffList;
-	 }
+    public static List getStaffDetails() throws IOException {
+        List<Staff> staffList = new ArrayList();
+        FileInputStream fis = null;
+
+        try {
+            fis = new FileInputStream(File_Path);
+            Workbook workbook = new XSSFWorkbook(fis);
+
+            int numberOfSheets = workbook.getNumberOfSheets();
+
+            for (int i = 0; i < numberOfSheets; i++) {
+                Sheet sheet = workbook.getSheetAt(i);
+                Iterator rowIterator = sheet.iterator();
+
+                while (rowIterator.hasNext()) {
+                    Staff staff;
+                    String name = "";
+                    String activity = "";
+                    String area = "";
+                    Focus focus = null;
+
+                    Row row = (Row) rowIterator.next();
+                    Iterator cellIterator = row.cellIterator();
+
+                    while (cellIterator.hasNext()) {
+                        Cell cell = (Cell) cellIterator.next();
+
+                        if (cell.getColumnIndex() == 0) {
+                            name = cell.getStringCellValue();
+                        }
+                        else if (cell.getColumnIndex() == 1) {
+                            activity = cell.getStringCellValue();
+                        }
+                        else if (cell.getColumnIndex() == 2) {
+                            area = cell.getStringCellValue();
+                        }
+                        else if (cell.getColumnIndex() == 3) {
+                            String f = cell.getStringCellValue().trim();
+
+                            if (f.equals("Dagon Studies"))
+                                focus = Focus.DS;
+                        }
+                    }
+
+                    if(focus == null)
+                        focus = Focus.CS;
+                    staff = new Staff(name, activity, area, focus);
+                    System.out.println(staff.toString());
+                    staffList.add(staff);
+                }
+            }
+
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return staffList;
+    }
 }
