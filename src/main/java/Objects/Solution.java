e5b25b91e2e422d5c7f4322f32fc10019833af5f
magnussanne <magnussanne@me.com>
2020-03-31 12:24:20 +0100

    solution class

--- src/main/java/Objects/Solution.java
+++ src/main/java/Objects/Solution.java
@@ -1,4 +1,31 @@
 package Objects;
 
-public class Solution {
+public class Solution implements Interface.Solution{
+    private Student student;
+    private Project project;
+
+    @Override
+    public void setStudent(Student student) {
+
+    }
+
+    @Override
+    public Student getStudent() {
+        return student;
+    }
+
+    @Override
+    public void setProject(Project project) {
+
+    }
+
+    @Override
+    public Project getProject() {
+        return project;
+    }
+
+    @Override
+    public float getEnergy() {
+        return 0;
+    }
 }
