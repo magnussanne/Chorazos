39a437a73a9406fc0fac1c90d0d9d80de99bbb69
Conor Dunne <conoredunne@gmail.com>
2020-04-20 18:16:28 +0100

    Adjusted GeneticAlgorithm file

--- src/main/java/Functions/GeneticAlgorithm.java
+++ src/main/java/Functions/GeneticAlgorithm.java
@@ -9,9 +9,10 @@ import java.util.*;
 
 public class GeneticAlgorithm {
     private double Population_Size = 1000;
-    List<SolutionPermutation> populationList = new ArrayList<>();
 
         public List<SolutionPermutation> generateInitialPopulation(List<Student> studentList, List<Project> projectList) {
+            List<SolutionPermutation> populationList = new ArrayList<>();
+
             for(int i = 0; i < Population_Size; i++){
                 List<Solution> solutionList = new ArrayList<Solution>();
 
@@ -22,5 +23,7 @@ public class GeneticAlgorithm {
                 SolutionPermutation sp = new SolutionPermutation(solutionList);
                 populationList.add(sp);
             }
+
+            return populationList;
         }
 }
