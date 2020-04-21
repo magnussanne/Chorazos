c56af3f061d1a2418e8e7f8ee301dfe6a6f456dd
Conor Dunne <conoredunne@gmail.com>
2020-04-21 12:31:45 +0100

    Added Genetic Algoritms base variables

--- src/main/java/Functions/GeneticAlgorithm.java
+++ src/main/java/Functions/GeneticAlgorithm.java
@@ -8,22 +8,25 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class GeneticAlgorithm {
-    private double Population_Size = 1000;
+    private final int P = 1000; //  Population size
+    private final int M = 10;   //  Cull bottom M%
+    private final int N = 10;   //  Mate top N%
+    private final int R = 10;   //  Stop after R iterations without improvement
 
-        public List<SolutionPermutation> generateInitialPopulation(List<Student> studentList, List<Project> projectList) {
-            List<SolutionPermutation> populationList = new ArrayList<>();
+    public List<SolutionPermutation> generateInitialPopulation(List<Student> studentList, List<Project> projectList) {
+        List<SolutionPermutation> populationList = new ArrayList<>();
 
-            for(int i = 0; i < Population_Size; i++){
-                List<Solution> solutionList = new ArrayList<Solution>();
+        for(int i = 0; i < P; i++){
+            List<Solution> solutionList = new ArrayList<Solution>();
 
-                for (Student s : studentList) {
-                    solutionList.add(new Solution(s, projectList, solutionList));
-                }
-
-                SolutionPermutation sp = new SolutionPermutation(solutionList);
-                populationList.add(sp);
+            for (Student s : studentList) {
+                solutionList.add(new Solution(s, projectList, solutionList));
             }
 
-            return populationList;
+            SolutionPermutation sp = new SolutionPermutation(solutionList);
+            populationList.add(sp);
         }
+
+        return populationList;
+    }
 }
