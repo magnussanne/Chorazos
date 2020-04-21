423dfbb9e38cd8fde5fdfdf3c75a3d1fcc67c699
magnussanne <magnussanne@me.com>
2020-04-21 16:47:18 +0100

    update

--- src/main/java/Functions/GeneticAlgorithm.java
+++ src/main/java/Functions/GeneticAlgorithm.java
@@ -55,7 +55,7 @@ public class GeneticAlgorithm {
 
         for(int i=0; i<numberToCull; i++) {
             population.remove(population.size()-1);
-        }s
+        }
     }
 
     private void breed(List<SolutionPermutation> population) {
