cdcba91d19c194242efbb4a7a2b02671bc649c8c
magnussanne <magnussanne@me.com>
2020-04-28 13:03:37 +0100

    tabbed panes

--- src/main/java/run.java
+++ src/main/java/run.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class run {
     public static void main(String[] args) {
@@ -6,5 +7,16 @@ public class run {
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainWindow.setSize(720, 500);
         mainWindow.setVisible(true);
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Genetic Algorithm", makePanel("Genetic Algorithm goes here"));
+        tabbedPane.addTab("Simulated Annealing", makePanel("Simulated Annealing goes here"));
+        tabbedPane.addTab("Hill Climbing", makePanel("Hill Climbing goes here"));
+        mainWindow.getContentPane().add(tabbedPane);
+    }
+    private static JPanel makePanel(String text) {
+        JPanel p = new JPanel();
+        p.add(new Label(text));
+        p.setLayout(new GridLayout(1, 1));
+        return p;
     }
 }
