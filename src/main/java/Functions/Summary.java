ad353e36a4d3d243bc32d572f7309b7966e702fb
Conor Dunne <conoredunne@gmail.com>
2020-05-06 12:21:58 +0100

    Added summary constructor and test function

--- src/main/java/Functions/Summary.java
+++ src/main/java/Functions/Summary.java
@@ -1,6 +1,27 @@
 package Functions;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Summary extends JPanel {
+    public Summary() {
+        setPreferredSize(new Dimension(500, 500));
+        setBackground(Color.WHITE);
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        Summary summary = new Summary();
+
+        SwingUtilities.invokeLater(() -> {
+            final JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setResizable(false);
+            frame.add(summary, BorderLayout.CENTER);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+
+    }
 }
