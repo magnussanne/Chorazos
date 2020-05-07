8bea21e92edb613b479a85a8d8a29f4c719a5db7
gearoidl <gearoid.lynch@ucdconnect.ie>
2020-05-07 13:41:31 +0100

    commit to pull

--- src/main/java/Functions/Summary.java
+++ src/main/java/Functions/Summary.java
@@ -34,9 +34,7 @@ public class Summary extends JPanel {
 
     public Summary(SolutionPermutation solution) {
         setPreferredSize(new Dimension(500, 500));
-        columnNames = new Object[]{"Student Number", "Student Name,", "Project ID", "Project Name", "Preference"};
-        model = new DefaultTableModel(columnNames, solutionSize);
-        model.addRow(columnNames);
+        model = createModel(solution);
         summaryTable = new JTable(model);
         this.add(summaryTable);
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
@@ -44,8 +42,20 @@ public class Summary extends JPanel {
         this.add(exportButton(solution));
     }
 
-    public void addEntries(SolutionPermutation solution) {
-        TableColumn column;
+    public DefaultTableModel createModel(SolutionPermutation solution) {
+        columnNames = new Object[]{"Student Number", "Student Name,", "Project ID", "Project Name", "Preference"};
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+        model.addRow(new Object[]{"Student Number", "Student Name,", "Project ID", "Project Name", "Preference"});
+        Object[] row;
+
+        for(Solution s : solution.getSolutionList()) {
+            Student student = s.getStudent();
+            Project project = s.getProject();
+            row = new Object[]{student.getNumber(), student.getName(), project.getId(), project.getTitle()};
+            model.addRow(row);
+        }
+
+        return model;
     }
 
     private JTextPane textSummary(String string) {
